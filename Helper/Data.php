74a59b6c61db576aae2caaa3bfcb19c466a335ed
Nicolas MEDINA <nicolas@magemontreal.com>
2019-12-02 16:21:39 -0500

    Added missing import

--- Helper/Data.php
+++ Helper/Data.php
@@ -5,6 +5,7 @@ namespace MageMontreal\HideDefaultStoreCode\Helper;
 use Magento\Framework\App\Helper\AbstractHelper;
 use Magento\Framework\App\Helper\Context;
 use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
 
 class Data extends AbstractHelper
 {
